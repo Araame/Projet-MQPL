d9dbc3d66e171bf466466f4b8e70b4fe10764e05
Solange883 <soldesages88@gmail.com>
2025-06-02 15:18:57 +0000

    Premier commit depuis IntelliJ

--- src/main/java/org/example/Boisson.java
+++ src/main/java/org/example/Boisson.java
@@ -1,4 +1,35 @@
 package org.example;
 
+
+import java.util.Objects;
+
 public class Boisson {
+    private String id;
+    private String nom;
+    private double prix;
+    private String description;
+
+    public Boisson(String id, String nom, double prix) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+
+
+
 }
--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.util.Date;
+import java.util.UUID; // Pour générer un ID unique
+
 public class Transaction {
+    private String id;
+    private Date date;
+    private double montantPaye;
+    private Boisson boissonAchetee;
+    private double monnaieRendue;
+
+    public Transaction(double montantPaye, Boisson boissonAchetee, double monnaieRendue) {
+        this.id = UUID.randomUUID().toString();
+        this.date = new Date();
+        this.montantPaye = montantPaye;
+        this.boissonAchetee = boissonAchetee;
+        this.monnaieRendue = monnaieRendue;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getMontantPaye() {
+        return montantPaye;
+    }
+
+    public Boisson getBoissonAchetee() {
+        return boissonAchetee;
+    }
+
+    public double getMonnaieRendue() {
+        return monnaieRendue;
+    }
+
+
 }
