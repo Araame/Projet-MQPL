b27fd797b0e5dc9c50cc7e7aa361a89c399ba0b5
Solange883 <soldesages88@gmail.com>
2025-06-02 23:37:04 +0000

    il reste et Stock et TypeUtilisateur

--- src/main/java/org/example/Utilisateur.java
+++ src/main/java/org/example/Utilisateur.java
@@ -1,12 +1,13 @@
 package org.example;
-import org.example.TypeUtilisateur;
+
 
 import java.util.UUID;
 
 public class Utilisateur {
-    String id;
-    String nom;
-    TypeUtilisateur type;
+    private String id;
+    private String nom;
+    private TypeUtilisateur type;
+
     public Utilisateur (String nom, TypeUtilisateur type){
         this.id = UUID.randomUUID().toString();
         this.nom = nom;
@@ -25,18 +26,15 @@ public class Utilisateur {
 
     public void rechargerDistributeur(Distributeur distributeur, Boisson boisson, int quantite){
 
-        if (this.type != TypeUtilisateur.CLIENT) {
+        if (this.type == TypeUtilisateur.PERSONNEL) {
+            distributeur.rechargerStock(boisson, quantite);
+            System.out.println(this.nom + " (" + this.type + ") a rechargé " + quantite + " unités de " + boisson.getNom() + " dans le distributeur " + ".");
+        }
+        else{
             System.out.println("Accès refusé : L'utilisateur '" + this.nom + "' (" + this.type + ") n'est pas autorisé à recharger les distributeurs.");
-            return;
         }
 
-        if (distributeur == null || boisson == null || quantite <= 0) {
-            System.out.println("Rechargement impossible : Informations manquantes ou quantité invalide (doit être > 0).");
-            return;
-        }
 
-        distributeur.rechargerStock(boisson, quantite);
 
-        System.out.println(this.nom + " (" + this.type + ") a rechargé " + quantite + " unités de " + boisson.getNom() + " dans le distributeur " + ".");
     }
 }
